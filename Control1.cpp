de7d165f08822a84214c0b21095cf5b7d792c265
Vadim <90693908+VadimLedvanov@users.noreply.github.com>
2023-10-06 14:47:39 +0300

    Add files via upload

---
+++ Control1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+    ofstream fout("test.tx");
+    if (!fout)
+    {
+        cout << "Файл открыть невозможно";
+        return 1;
+    }
+    
+    string inputString;
+    getline(cin, inputString);
+    while (inputString.size() != 0)
+    { 
+        fout << inputString << endl;
+        getline(cin, inputString);
+    }
+}
---
+++ Control2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+
+    ofstream fout("test");
+    if (!fout.is_open())
+    {
+        cout << "Файл не удалось открыть.";
+        return 1;
+    }
+
+    const int N = 10;
+    int a[N] = { 1, 25, 6, 32, 43, 5, 96, 23, 4, 55 };
+
+    fout << "Исходный массив: ";
+    for (int i = 0; i < N; i++)
+    {
+        fout << a[i] << " ";
+    }
+    
+    int min = 0;
+    int buf = 0;
+
+    // сортировка выбором
+    for (int i = 0; i < N; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < N; j++)
+            min = (a[j] < a[min]) ? j : min;
+
+        if (i != min)
+        {
+            buf = a[i];
+            a[i] = a[min];
+            a[min] = buf;
+        }
+    }
+
+    fout << "\nОтсортированный массив: ";
+    for (int i = 0; i < N; i++)
+    {
+        fout << a[i] << " ";
+    }
+    fout.close();
+
+    for (int i : a)
+        cout << i << '\t';
+}
